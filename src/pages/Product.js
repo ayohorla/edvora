e83f1c2c441cd731d1789d788514fa16b1319320
ayohorla <forlecforlan@gmail.com>
2021-12-29 22:10:23 +0100

    list

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductList from "../products/ProductList";
 
 function Product (){
 
     const [isloading, setIsLoading] =useState();
-    const [loadedProducts, setLoadedProducts] = useState();
+    const [loadedProducts, setLoadedProducts] = useState([]);
 
-    
+    useEffect (() => {
+        setIsLoading (true);
+        fetch(
+            'https://assessment-edvora.herokuapp.com'
+        )
+        .then((response => {
+            return response.json();
+        })
+        .then((data =>{
+            const products = [];
+        })))
+    })
+
+
     return(
         <section>
             <h1>Product Name</h1>
